57b21dd6238e894b2eb0f7990527b0cc82ee2263
Chris <77562903+darksaber9999@users.noreply.github.com>
2022-11-08 23:31:26 -0700

    Build out functionality to set product data to state

--- src/components/ShopperWindow.jsx
+++ src/components/ShopperWindow.jsx
@@ -5,15 +5,46 @@ import AuthWindow from "../components/AuthWindow";
 import Shipping from "../components/Shipping";
 import Payment from "../components/Payment";
 import Confirm from "../components/Confirm";
+import CommerceService from "../services";
 import { INITIAL_DISPLAY } from "../constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
+const commerce = new CommerceService();
 class ShopperWindow extends React.Component {
   state = {
     display: INITIAL_DISPLAY,
     loading: false,
     error: false,
+    data: [],
+  }
+
+  setProductData = () => {
+    this.setState({
+      loading: true,
+      error: false,
+    });
+    commerce.fetchProducts().then((res) => {
+      if (res && res.response.ok) {
+        this.setState({
+          data: res.data,
+          loading: false,
+        });
+      } else {
+        this.setState({ loading: false });
+      }
+    }, (error) => {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: true,
+        data: [],
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.setProductData();
   }
 
   toggleDisplay = (name) => this.setState((prevState) => ({ display: { ...prevState.display, [name]: !prevState.display[name] } }));
@@ -21,7 +52,7 @@ class ShopperWindow extends React.Component {
   toggleCart = () => this.toggleDisplay('cart');
 
   render() {
-    const { display: { store, cart, authWindow, shipping, payment, confirm } } = this.state;
+    const { display: { store, cart, authWindow, shipping, payment, confirm }, loading, error, data } = this.state;
 
     return (
       <>
@@ -36,7 +67,11 @@ class ShopperWindow extends React.Component {
         </header>
         <div className="component-window">
           {store ?
-            <Store />
+            <Store
+              loading={loading}
+              data={data}
+              error={error}
+            />
             : null}
           {authWindow ?
             <AuthWindow />
