6dfbacd7439badbaf8f023041552da9a2f8d8473
Chris <77562903+darksaber9999@users.noreply.github.com>
2022-11-28 00:45:16 -0700

    Add Payment validations

--- src/validations.js
+++ src/validations.js
@@ -51,3 +51,26 @@ export const checkForDuplicateUser = (value, userArray) => {
 }
 
 export const passwordMatchValidation = (password1, password2) => (password1 !== password2) ? 'Passwords must match' : undefined;
+
+export const cardNumberValidation = (cardNumber) => {
+  const regexPattern = {
+    MASTERCARD: /^5[1-5][0-9]{1,}|^2[2-7][0-9]{1,}$/,
+    VISA: /^4[0-9]{2,}$/,
+    AMERICAN_EXPRESS: /^3[47][0-9]{5,}$/,
+    DISCOVER: /^6(?:011|5[0-9]{2})[0-9]{3,}$/,
+  };
+
+  for (const card in regexPattern) {
+    if (cardNumber.replace(/[^\d]/g, '').match(regexPattern[card])) {
+      if (cardNumber) {
+        return cardNumber && /^[1-6]{1}[0-9]{14,15}$/i.test(cardNumber.replace(/[^\d]/g, '').trim())
+          ? undefined
+          : 'Enter a valid Card';
+      }
+    }
+  }
+
+  return 'Enter a valid Card';
+};
+
+export const securityCodeValidation = (min, value) => (value && value.length < min) ? 'Must be 3 characters or more' : undefined;
