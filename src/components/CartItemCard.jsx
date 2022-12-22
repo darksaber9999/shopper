64e93233332f96f6ced5fca0df90bd49d1806fd8
Chris <77562903+darksaber9999@users.noreply.github.com>
2022-12-21 20:49:41 -0700

    Added handler for Quantity Controls

--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -5,11 +5,13 @@ import { faCaretDown, faCaretRight, faMinus, faPlus, faXmark } from "@fortawesom
 
 const CartItemCard = ({ data, product, toggleHiddenIcons, removeFromCart, changeQuantity }) => {
 
-  const toggleQuantityControls = (e) => {
+  const toggleQuantityControls = (element) => {
     toggleHiddenIcons(`caret-right-${product.id}`);
     toggleHiddenIcons(`caret-down-${product.id}`);
-    e.target.parentElement.parentElement.children[5].classList.toggle('hidden');
+    element.classList.toggle('hidden');
   };
+
+  const handleQuantityControls = (e) => toggleQuantityControls(e.target.parentElement.parentElement.children[5])
 
   const handleRemoveFromCart = (e) => removeFromCart(e.target.parentElement.parentElement.parentElement.dataset.item);
 
@@ -21,35 +23,33 @@ const CartItemCard = ({ data, product, toggleHiddenIcons, removeFromCart, change
   return (
     <>
       {data.filter((item) => item.id === product.id).map((cartItem) => (
-        <>
-          <div key={cartItem.id} className="cart-item" data-item={cartItem.id}>
-            <FontAwesomeIcon
-              icon={faCaretRight}
-              onClick={toggleQuantityControls}
-              id={`caret-right-${cartItem.id}`}
-              className="caret-right"
-            />
-            <FontAwesomeIcon
-              icon={faCaretDown}
-              onClick={toggleQuantityControls}
-              id={`caret-down-${cartItem.id}`}
-              className={`caret-down hidden`}
-            />
-            <span className="cart-item-name">{cartItem.name}</span>
-            <span className="cart-item-quantity">Qty: {product.quantity}</span>
-            <span className="cart-item-total">${(cartItem.price * product.quantity).toFixed(2)}</span>
+        <div key={cartItem.id} className="cart-item" data-item={cartItem.id}>
+          <FontAwesomeIcon
+            icon={faCaretRight}
+            onClick={handleQuantityControls}
+            id={`caret-right-${cartItem.id}`}
+            className="caret-right"
+          />
+          <FontAwesomeIcon
+            icon={faCaretDown}
+            onClick={handleQuantityControls}
+            id={`caret-down-${cartItem.id}`}
+            className={`caret-down hidden`}
+          />
+          <span className="cart-item-name">{cartItem.name}</span>
+          <span className="cart-item-quantity">Qty: {product.quantity}</span>
+          <span className="cart-item-total">${(cartItem.price * product.quantity).toFixed(2)}</span>
 
-            <div className="cart-item-quantity-controls hidden">
-              <FontAwesomeIcon icon={faXmark} onClick={handleRemoveFromCart} />
-              <button className="cart-item-quantity-button" onClick={handleChangeQuantityRemove}>
-                <FontAwesomeIcon icon={faMinus} />
-              </button>
-              <button className="cart-item-quantity-button" onClick={handleChangeQuantityAdd}>
-                <FontAwesomeIcon icon={faPlus} />
-              </button>
-            </div>
+          <div className="cart-item-quantity-controls hidden">
+            <FontAwesomeIcon icon={faXmark} onClick={handleRemoveFromCart} />
+            <button className="cart-item-quantity-button" onClick={handleChangeQuantityRemove}>
+              <FontAwesomeIcon icon={faMinus} />
+            </button>
+            <button className="cart-item-quantity-button" onClick={handleChangeQuantityAdd}>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
           </div>
-        </>
+        </div>
       ))}
     </>
   )
